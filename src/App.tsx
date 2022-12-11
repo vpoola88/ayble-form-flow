f9ec387365a0ceb3cad374ef01f1714ea19a91ed
Vivek Poola <1377918+vpoola88@users.noreply.github.com>
2022-12-10 19:48:09 -0800

    loop through form questions and appropriately display form, use handle change to get form question value back and form answer id back, start useEffect to determine which index should be enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import OnboardingForm from "./components/OnboardingForm";
-import PrimaryButton from "./components/PrimaryButton";
 import TopHeader from "./components/TopHeader";
 
 function App() {
@@ -9,22 +8,21 @@ function App() {
       <main>
         <div className="bg-primary">
           <section className="max-w-7xl mx-auto relative">
-            <div className="px-12 py-32">
+            <div className="px-12 sm:py-32 py-12">
               <h1 className="font-playfair text-5xl pb-12 text-white tracking-wide">
                 Welcome to Ayble's onboarding form.
               </h1>
               <div className="max-w-5xl">
-                <p className="text-white text-2xl mb-8">
+                <p className="text-white text-2xl sm:mb-8">
                   Since you have indicated you may have Ulcerative Colitis,
                   please answer the following questions as closely to your
                   knowledge for us to best understand how your feeling.
                 </p>
-                <PrimaryButton />
               </div>
             </div>
             <img
               src="https://uploads-ssl.webflow.com/6349784a667b342cb347c1aa/6358a0c337c8c33ce1cda747_Riso%20-%20Square.svg"
-              className="absolute top-40 invert contrast-50 opacity-20 w-[380px] left-[-14%]"
+              className="absolute top-10 invert contrast-50 opacity-20 w-[380px] sm:left-[-14%] left-[-60%]"
             />
           </section>
         </div>
--- src/components/OnboardingForm.tsx
+++ src/components/OnboardingForm.tsx
@@ -1,103 +1,162 @@
-import { useState } from "react";
+import PreviousMap from "postcss/lib/previous-map";
+import { useEffect, useState } from "react";
+
+const questions = [
+  {
+    question: {
+      id: "onboarding-uc-1",
+      question:
+        "On average, how mnay bowl movements did you have each day (during the daytime) over the past 7 days (1 week)?",
+    },
+    answers: [
+      { id: "onboarding-uc-ans-1-03", response: "0-3 stools/day", score: 0 },
+      { id: "onboarding-uc-ans-1-46", response: "4-6 stools/day", score: 1 },
+      { id: "onboarding-uc-ans-1-79", response: "7-9 stools/day", score: 2 },
+    ],
+  },
+  {
+    question: {
+      id: "onboarding-uc-2",
+      question:
+        "On average, how mnay bowl movements did you have each night (during the night) over the past 7 days (1 week)?",
+    },
+    answers: [
+      { id: "onboarding-uc-ans-2-0", response: "0 stools/day", score: 0 },
+      { id: "onboarding-uc-ans-2-13", response: "1-3 stools/day", score: 1 },
+      { id: "onboarding-uc-ans-2-46", response: "4-6 stools/day", score: 2 },
+    ],
+  },
+  {
+    question: {
+      id: "onboarding-uc-3",
+      question:
+        "How urgent were your bowel movements on average over the past 7 days (1 week)?",
+    },
+    answers: [
+      { id: "onboarding-uc-ans-3-urgency", response: "No urgency", score: 0 },
+      { id: "onboarding-uc-ans-3-hurry", response: "I had to hurry", score: 1 },
+      {
+        id: "onboarding-uc-ans-3-go",
+        response: "I had to go immediately",
+        score: 2,
+      },
+      { id: "onboarding-uc-ans-3-in", response: "I was incontent", score: 3 },
+    ],
+  },
+  {
+    question: {
+      id: "onboarding-uc-4",
+      question:
+        "On average how much blood was present in your stool over the past 7 days (1 week)?",
+    },
+    answers: [
+      { id: "onboarding-uc-ans-4-none", response: "None", score: 0 },
+      { id: "onboarding-uc-ans-4-trace", response: "Trace amounts", score: 1 },
+      {
+        id: "onboarding-uc-ans-4-mod",
+        response: "A moderate amount (but less than half the time)",
+        score: 2,
+      },
+      {
+        id: "onboarding-uc-ans-4-lot",
+        response: "A lot (more than half the time)",
+        score: 3,
+      },
+    ],
+  },
+  {
+    question: {
+      id: "onboarding-uc-5",
+      question:
+        "How woulud you rate your general wellbeing over the past 7 days (1 week)? 0 (worst) to 10 (best)",
+    },
+    answers: [
+      {
+        id: "onboarding-uc-ans-5-very-well",
+        response: "Very well (>7)",
+        score: 0,
+      },
+      {
+        id: "onboarding-uc-ans-5-par",
+        response: "Slightly below par (6)",
+        score: 1,
+      },
+      { id: "onboarding-uc-ans-5-poor", response: "Poor (5)", score: 2 },
+      {
+        id: "onboarding-uc-ans-5-very-poor",
+        response: "Very poor (4)",
+        score: 3,
+      },
+      {
+        id: "onboarding-uc-ans-5-terrible",
+        response: "Terrible (<4)",
+        score: 4,
+      },
+    ],
+  },
+];
 
 function OnboardingForm() {
-  const [formData, setFormData] = useState({
-    formQuestion: "",
-    formAnswer: "",
-    formAnswerValue: "",
-  });
+  const [formData, setFormData] = useState({});
+  const [formStep, setFormStep] = useState(0);
 
-  const [formInputStatus, setFormInputStatus] = useState(1);
+  const [formInputStatus, setFormInputStatus] = useState(0);
+
+  useEffect(() => {
+    let status = questions.length;
+    for (var i = 0; i <= questions.length; i++) {
+      if (i > 0) {
+        // set disabled class here depending on the index of the questions. I didn't have time to finish this, but
+        // I think we can take the index, and disable everything except the current index. Once the current index
+        // changes, then we would take i + 1 and enable that next one.
+      }
+    }
+  }, [formInputStatus]);
 
   function handleChange(e) {
-    console.log("question" + e.target?.value);
-    setFormInputStatus(2);
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   }
 
   function handleSubmit(e) {
     e.preventDefault();
+    // we can error handle here
+
+    // if no errors
     console.log("submitted");
+    console.log(formData);
   }
 
   return (
     <div className="max-w-3xl mx-auto">
       <form onSubmit={(e) => e.preventDefault()}>
         <ul>
-          <li className="bg-gray-50 p-8 mb-4">
-            <p id="onboarding-uc-1" className="text-2xl">
-              On average, how many bowel movements did you have each day (during
-              the daytime) over the past 7 days (1 week)?:
-            </p>
-            <div className="flex flex-col space-y-2 text-xl pt-8">
-              <label
-                id="onboarding-uc-ans-1-03"
-                onClick={(e) => handleChange(e)}
-              >
-                <input name="onboarding-uc-1" type="radio" value="0" />
-                <span className="px-2">0-2 stools/day</span>
-              </label>
-              <label
-                id="onboarding-uc-ans-1-46"
-                onClick={(e) => handleChange(e)}
-              >
-                <input name="onboarding-uc-1" type="radio" value="1" />
-                <span className="px-2">4-6 stools/day</span>
-              </label>
-              <label
-                id="onboarding-uc-ans-1-79"
-                onClick={(e) => handleChange(e)}
-              >
-                <input name="onboarding-uc-1" type="radio" value="2" />
-                <span className="px-2">7-9 stools/day</span>
-              </label>
-              <label
-                id="onboarding-uc-ans-1-10-more"
-                onClick={(e) => handleChange(e)}
-              >
-                <input name="onboarding-uc-1" type="radio" value="3" />
-                <span className="px-2">10+ stools/day</span>
-              </label>
-            </div>
-          </li>
-
-          <li
-            className={`"bg-gray-50 p-8 mb-4" ${
-              formInputStatus > 1 ? "" : "opacity-60 cursor-not-allowed"
-            }`}
-          >
-            <p id="onboarding-uc-2" className="text-2xl">
-              On average, how many bowel movements did you have each night
-              (during the night) over the past 7 days (1 week)?:
-            </p>
-            <div className="flex flex-col space-y-2 text-xl pt-8">
-              <label
-                id="onboarding-uc-ans-2-0"
-                onClick={(e) => handleChange(e)}
-              >
-                <input
-                  disabled={formInputStatus > 2 ? true : false}
-                  name="onboarding-uc-2"
-                  type="radio"
-                  value="0"
-                />
-                <span className="px-2">0 stools/night</span>
-              </label>
-              <label
-                id="onboarding-uc-ans-2-13"
-                onClick={(e) => handleChange(e)}
-              >
-                <input name="onboarding-uc-2" type="radio" value="1" />
-                <span className="px-2">1-3 stools/night</span>
-              </label>
-              <label
-                id="onboarding-uc-ans-2-46"
-                onClick={(e) => handleChange(e)}
-              >
-                <input name="onboarding-uc-2" type="radio" value="2" />
-                <span className="px-2">4-6 stools/night</span>
-              </label>
-            </div>
-          </li>
+          {questions.map((question, index) => (
+            <li
+              key={index}
+              className={`${
+                index > 0 ? "opacity-60 cursor-not-allowed" : ""
+              } bg-gray-50 p-8 mb-4`}
+            >
+              <p id="onboarding-uc-1" className="text-2xl">
+                {question.question.question}
+              </p>
+              <div className="flex flex-col space-y-2 text-xl pt-8">
+                <label id={question.question.id} onClick={handleChange}>
+                  {question.answers.map((answer, index) => (
+                    <div key={index}>
+                      <input
+                        type="radio"
+                        name={question.question.id}
+                        value={answer.id}
+                      />
+                      <span className="px-2">{answer.response}</span>
+                    </div>
+                  ))}
+                </label>
+              </div>
+            </li>
+          ))}
         </ul>
         <button
           className="bg-secondary px-12 py-4 my-4 rounded-full cursor-pointer border-2 border-secondary text-lg
